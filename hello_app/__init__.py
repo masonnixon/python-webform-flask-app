09783e0f7015a993b547a8de2891c3814971850a
Mason <masonnixon@gmail.com>
2021-03-17 21:04:19 -0500

    Added better key management. Updated notes.

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask  # Import the Flask class
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)    # Create an instance of the class for our use
 
 # Flask-WTF requires an encryption key - the string can be anything
-app.config['SECRET_KEY'] = 'Du4wHBTUKeelbceT5w3nBh6tXlH68VFE'
+# Upon deployment, we set the key in the server environment variable, SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'Du4wHBTUKeelbceT5w3nBh6tXlH68VFE'
 
 # Flask-Bootstrap requires this line
 Bootstrap(app)
